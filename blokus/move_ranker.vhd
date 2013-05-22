ad258928d4ea5b9354b2d4c44bfb19fb4ee32f9a
Andrew R <andrewreds@gmail.com>
2013-05-22 11:36:43 +1000

    small change
    Signed-off-by: Andrew R <andrewreds@gmail.com>

--- blokus/move_ranker.vhd
+++ blokus/move_ranker.vhd
@@ -3,13 +3,19 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 use work.types.all;
 
-
 entity move_ranker is
 	Port(
-		our_window : in board_window_7;
-		opponent_window: in board_window_7;
-		move_window : in board_window_7;
-		
-		raiting : out std_logic_vector(31 downto 0)
+		our_window      : in  board_window_7;
+		opponent_window : in  board_window_7;
+		move_window     : in  std_logic_vector(24 downto 0);
+
+		raiting         : out std_logic_vector(31 downto 0)
 	);
 end entity move_ranker;
+
+architecture structural of move_ranker is
+
+begin
+
+
+end structural;
